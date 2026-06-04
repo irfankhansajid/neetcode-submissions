d0c1c1ce8cc4b70b81c03496d28e9112a676d40f
Irfan Khan Sajid <56930422+irfankhansajid@users.noreply.github.com>
2026-06-04 21:38:23 +0600

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> duplicate = new HashSet<>();
+
+
+        for (int i = 0; i<nums.length; i++) {
+            if (duplicate.contains(nums[i])) {
+                return true;
+            }
+            duplicate.add(nums[i]);
+        }
+        
+        return false;
+    }
+}
